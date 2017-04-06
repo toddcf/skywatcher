63bda247b6aece761142bbe4f08219d3e8eb48d0
Todd Croak-Falen <toddcf@gmail.com>
2017-04-06 16:14:21 -0700

    Moved variables inside getWeather function. Using sample API key until I can figure out how to securely store my actual API key on the server side.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,48 +1,48 @@
 $( document ).ready( function() {
 
-	var xmlMode = "&xml";
-	// Default is JSON. Concatonate xmlMode after city if you want to return XML instead.
-
-	// URL Head:
-	var url = "http://api.openweathermap.org/data/2.5/weather?";
-
-	// Default countryCode is United States.
-	var countryCode = "us";
-
-	// Location by City:
-	var userCityName;
-	var cityName = "q=" + userCityName + "," + countryCode;
-	var userCityId;
-	var cityID = "id=" + userCityId;
-
-	// Location by Coordinates:
-	var lat;
-	var lon;
-	var geo = "lat=" + lat + "&lon=" + lon;
-
-	// Location by Zip:
-	var userZip;
-	var zip = "zip=" + userZip + "," + countryCode;
-
-	// Units:
-	var metric = "&units=metric"; // aka Celcius
-	var imperial = "&units=imperial"; // aka Fahrenheit
-	// If units are not specified, default is "standard," aka Kelvin.
-
-	// API Key:
-	var key = "&appid=OBFUSCATE";
-
-	var apiQuery = url + cityName + imperial + key;
-
-	console.log(apiQuery);
-
 	// AJAX Call:
 	function getWeather() {
 		var city = $( "#city" ).val();
 
+		var xmlMode = "&xml";
+		// Default is JSON. Concatonate xmlMode after city if you want to return XML instead.
+
+		// URL Head:
+		var url = "http://api.openweathermap.org/data/2.5/weather?";
+
+		// Default countryCode is United States.
+		var countryCode = "us";
+
+		// Location by City:
+		// var userCityName;
+		// var cityName = "q=" + userCityName + "," + countryCode;
+		var userCityId;
+		var cityID = "id=" + userCityId;
+
+		// Location by Coordinates:
+		var lat;
+		var lon;
+		var geo = "lat=" + lat + "&lon=" + lon;
+
+		// Location by Zip:
+		var userZip;
+		var zip = "zip=" + userZip + "," + countryCode;
+
+		// Units:
+		var metric = "&units=metric"; // aka Celcius
+		var imperial = "&units=imperial"; // aka Fahrenheit
+		// If units are not specified, default is "standard," aka Kelvin.
+
+		// API Key:
+		var sample = "&appid=170e406aaa5d1d76d71ee5c974d5ed32";
+		var key = "&appid=OBFUSCATE";
+
 		// Only if "city" is not empty:
 		if ( city != "" ) {
+			$.ajax( {
+				url: "http://api.openweathermap.org/data/2.5/weather?" + city + imperial + sample;
 
+			});
 		}
 		// If "city" is empty:
 		else {
