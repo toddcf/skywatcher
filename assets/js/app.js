dabccf44affa7f13fa886b9212c9eecd4c1816e6
Todd Croak-Falen <toddcf@gmail.com>
2017-06-13 16:35:36 -0700

    Revised footer date display.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,13 @@ $( document ).ready( function() {
 	// Pass current year into footer copyright:
 	var thisYear = new Date().getFullYear();
 
-	// Footer (IIFE):
-	var footerInfo = ( function() {
-		$( ".currentYear" ).html( " &ndash; " + thisYear + " " );
-	}) ();
+	if ( thisYear > 2017 ) {
+		// Footer (IIFE):
+		var footerInfo = ( function() {
+			$( ".currentYear" ).html( " &ndash; " + thisYear + " " );
+		}) ();
+	}
+
+
 
 } );
